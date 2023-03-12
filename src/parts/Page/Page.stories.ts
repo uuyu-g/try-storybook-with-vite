f279ea9196780cf73eed239bc5e72e34eea00fc1
Yugo Maki <36399275+uuyu-g@users.noreply.github.com>
2023-03-12 18:53:39 +0900

    場所移動

--- src/parts/Page/Page.stories.ts
+++ src/parts/Page/Page.stories.ts
@@ -4,12 +4,11 @@ import { within, userEvent } from '@storybook/testing-library';
 import { Page } from './Page';
 
 const meta = {
-  title: 'Example/Page',
-  component: Page,
-  parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/7.0/react/configure/story-layout
-    layout: 'fullscreen',
-  },
+	component: Page,
+	parameters: {
+		// More on how to position stories at: https://storybook.js.org/docs/7.0/react/configure/story-layout
+		layout: 'fullscreen',
+	},
 } satisfies Meta<typeof Page>;
 
 export default meta;
@@ -19,11 +18,11 @@ export const LoggedOut: Story = {};
 
 // More on interaction testing: https://storybook.js.org/docs/7.0/react/writing-tests/interaction-testing
 export const LoggedIn: Story = {
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    const loginButton = await canvas.getByRole('button', {
-      name: /Log in/i,
-    });
-    await userEvent.click(loginButton);
-  },
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement);
+		const loginButton = await canvas.getByRole('button', {
+			name: /Log in/i,
+		});
+		await userEvent.click(loginButton);
+	},
 };
